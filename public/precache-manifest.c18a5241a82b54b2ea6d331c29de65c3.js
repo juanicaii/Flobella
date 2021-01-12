752ebc41bb4a0bb8b0dd42fb1d3388f6ccc5bacc
juaniseijas <juaniseijas00@gmail.com>
2021-01-12 09:20:26 -0300

    fix

---
+++ public/precache-manifest.c18a5241a82b54b2ea6d331c29de65c3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4cd40c2f03492f0b58d49731dfbe35ab",
+    "url": "/index.html"
+  },
+  {
+    "revision": "039b5db6eca2f719d87d",
+    "url": "/static/css/main.633bb7fc.chunk.css"
+  },
+  {
+    "revision": "a662bbbc6dbb040d1059",
+    "url": "/static/js/2.afe5d98f.chunk.js"
+  },
+  {
+    "revision": "2069192f75c6f3fb6247231eea52f248",
+    "url": "/static/js/2.afe5d98f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "039b5db6eca2f719d87d",
+    "url": "/static/js/main.c1d45357.chunk.js"
+  },
+  {
+    "revision": "23f7e4d2c8397ce55bf6",
+    "url": "/static/js/runtime-main.5fe610d3.js"
+  },
+  {
+    "revision": "6d4e05823697958d8f798ee6f7cf3881",
+    "url": "/static/media/Aileron-Black.6d4e0582.otf"
+  },
+  {
+    "revision": "56bef66f66057990468ce5ed2397ea2b",
+    "url": "/static/media/logoJS.56bef66f.otf"
+  }
+]);
